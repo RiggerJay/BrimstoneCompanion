c7c448b31bc8cf7f9589e77c23069f7b637e2313
Guy Symonds <Guy@redspartan.co.uk>
2022-09-01 01:52:01 +0100

    Added Domain Layer

---
+++ src/Domain/Models/Attribute.cs
@@ -0,0 +1,8 @@
+﻿namespace BrimstoneCompanion.Domain.Models
+{
+    public class Attribute
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Value { get; set; }
+    }
+}
---
+++ src/Domain/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace BrimstoneCompanion.Domain.Models
+{
+    public class Character
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Class { get; set; } = string.Empty;
+
+        public IDictionary<string, Attribute> Attributes { get; set; }
+            = new Dictionary<string, Attribute>();
+    }
+}
