2b80e7f48647a6d149ba74df3ec3d358eb1f5e3a
Guy Symonds <Guy@redspartan.co.uk>
2022-09-01 01:56:22 +0100

    Added Application Layer

---
+++ src/Application/ObservableModels/ObservableCharacter.cs
@@ -0,0 +1,26 @@
+﻿using BrimstoneCompanion.Domain.Models;
+
+namespace BrimstoneCompanion.Application.ObservableModels
+{
+    public class ObservableCharacter
+    {
+        private readonly Character _character;
+
+        public ObservableCharacter(Character character)
+        {
+            _character = character ?? throw new ArgumentNullException(nameof(character));
+        }
+
+        public string Name
+        {
+            get => _character.Name;
+            set => _character.Name = value;
+        }
+
+        public string Class
+        {
+            get => _character.Class;
+            set => _character.Class = value;
+        }
+    }
+}
