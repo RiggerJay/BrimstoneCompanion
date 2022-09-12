46be4ad8537e730539927b12c23b8ec73291a873
Guy Symonds <guy@redspartan.co.uk>
2022-09-12 17:20:02 +0100

    Names are showing ok for Features

--- src/Presentation/MauiUI/ViewModels/NewFeatureViewModel.cs
+++ src/Presentation/MauiUI/ViewModels/NewFeatureViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using MediatR;
+using RedSpartan.BrimstoneCompanion.AppLayer.Interfaces;
 using RedSpartan.BrimstoneCompanion.AppLayer.ObservableModels;
 using RedSpartan.BrimstoneCompanion.Domain;
 using RedSpartan.BrimstoneCompanion.Domain.Models;
@@ -10,19 +11,34 @@ namespace RedSpartan.BrimstoneCompanion.MauiUI.ViewModels
     public partial class NewFeatureViewModel : ViewModelBase
     {
         private readonly IMediator _mediator;
+        private readonly ITextResource _textResource;
         private string? _selectedProperty;
         private int? _value;
+        private readonly IDictionary<string, string> _properties = new Dictionary<string, string>();
 
-        public NewFeatureViewModel(IMediator mediator)
+        public NewFeatureViewModel(IMediator mediator
+            , ITextResource textResource)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _textResource = textResource ?? throw new ArgumentNullException(nameof(textResource));
+            UpdateProperties();
+        }
+
+        private void UpdateProperties()
+        {
+            foreach (var item in AttributeNames.Strings)
+            {
+                Properties.Add(_textResource.GetValue(item));
+                _properties.Add(_textResource.GetValue(item), item);
+            }
+            OnPropertyChanged(nameof(Properties));
         }
 
         public ObservableFeature Feature { get; } = ObservableFeature.New();
 
         public IList<string> Types => Enum.GetNames(typeof(FeatureTypes));
 
-        public IList<string> Properties { get; } = AttributeNames.Strings;
+        public IList<string> Properties { get; } = new List<string>();
 
         public string? SelectedProperty { get => _selectedProperty; set => SetProperty(ref _selectedProperty, value); }
 
@@ -31,10 +47,10 @@ namespace RedSpartan.BrimstoneCompanion.MauiUI.ViewModels
         [RelayCommand]
         public async Task SaveAndClose()
         {
-            if (!string.IsNullOrEmpty(SelectedProperty)
+            if (!string.IsNullOrWhiteSpace(SelectedProperty)
                 && (Value != null && Value != 0))
             {
-                Feature.Properties.Add(SelectedProperty, (int)Value);
+                Feature.Properties.Add(_properties[SelectedProperty], (int)Value);
             }
 
             await _mediator.Send(NavRequest.Close(Feature));
