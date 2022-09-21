a6c772c5b6180c9bf449533213c23e9cc6cf79f0
Guy Symonds <guy@redspartan.co.uk>
2022-09-21 19:54:31 +0100

    Added reset for edit

--- src/Presentation/MauiUI/Pages/EditFeaturePage.xaml.cs
+++ src/Presentation/MauiUI/Pages/EditFeaturePage.xaml.cs
@@ -6,13 +6,20 @@ namespace RedSpartan.BrimstoneCompanion.MauiUI.Pages;
 public partial class EditFeaturePage : ContentPage
 {
     private readonly ITextResource _textResource;
+    private readonly EditFeatureViewModel _viewModel;
 
     public EditFeaturePage(EditFeatureViewModel viewModel, ITextResource textResource)
     {
         _textResource = textResource ?? throw new ArgumentNullException(nameof(textResource));
-        BindingContext = viewModel;
+        BindingContext = _viewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
         InitializeComponent();
     }
 
     public ITextResource TextResource => _textResource;
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _viewModel.Reset();
+    }
 }
