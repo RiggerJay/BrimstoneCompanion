1d29cbaae2ef9fad1b4ea2f851c2fb0c112cb3ab
Guy Symonds <guy@redspartan.co.uk>
2022-09-22 13:26:29 +0100

    Added more to Character Page

---
+++ src/Presentation/MauiUI/Converters/StringToSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace RedSpartan.BrimstoneCompanion.MauiUI.Converters
+{
+    public class StringToSizeConverter : IValueConverter
+    {
+        public Enum Enum { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return e.ToString();
+            }
+            return 26;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
