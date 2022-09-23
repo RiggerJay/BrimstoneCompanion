584d5250d547ad0edca30a4a594515b0d401c769
Guy Symonds <guy@redspartan.co.uk>
2022-09-23 14:43:34 +0100

    Updated bool to null

---
+++ src/Presentation/MauiUI/Converters/NotIntToBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Globalization;
+
+namespace RedSpartan.BrimstoneCompanion.MauiUI.Converters
+{
+    public class NotIntToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            _ = int.TryParse(parameter?.ToString() ?? string.Empty, out int count);
+
+            if (value is IList list)
+            {
+                return list.Count != count;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
