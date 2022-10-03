3db2fcb0973ba32c7bd54fa45afe0b7ea1131fe9
Guy Symonds <Guy@redspartan.co.uk>
2022-10-03 17:32:12 +0100

    Sidebags now lose items when the bag shrinks

--- src/Infrastructure/Handlers/CreateFeatureHandler.cs
+++ src/Infrastructure/Handlers/CreateFeatureHandler.cs
@@ -4,23 +4,24 @@ using RedSpartan.BrimstoneCompanion.AppLayer.Interfaces;
 using RedSpartan.BrimstoneCompanion.Domain;
 using RedSpartan.BrimstoneCompanion.Infrastructure.Messages;
 using RedSpartan.BrimstoneCompanion.Infrastructure.Requests;
-using RedSpartan.BrimstoneCompanion.Infrastructure.Services;
+using System;
 
 namespace RedSpartan.BrimstoneCompanion.Infrastructure.Handlers
 {
-    public class AddFeatureHandler : IRequestHandler<AddFeatureRequest, bool>
+    public class CreateFeatureHandler : IRequestHandler<CreateFeatureRequest, bool>
     {
         private readonly IMessenger _messenger;
         private readonly IApplicationState _applicationState;
+        private readonly Random _random = new();
 
-        public AddFeatureHandler(IApplicationState applicationState,
+        public CreateFeatureHandler(IApplicationState applicationState,
             IMessenger messenger)
         {
             _messenger = messenger ?? throw new ArgumentNullException(nameof(messenger));
             _applicationState = applicationState ?? throw new ArgumentNullException(nameof(applicationState));
         }
 
-        public Task<bool> Handle(AddFeatureRequest request, CancellationToken cancellationToken)
+        public Task<bool> Handle(CreateFeatureRequest request, CancellationToken cancellationToken)
         {
             _applicationState.Character.Features.Add(request.Feature);
 
@@ -31,22 +32,29 @@ namespace RedSpartan.BrimstoneCompanion.Infrastructure.Handlers
                 var attribute = _applicationState.Character.Attributes.First(x => x.Key == prop.Key);
 
                 attribute.SetCurrentValues(_applicationState.Character.Features);
-
-                if(attribute.Key == AttributeNames.SIDEBAG
-                    && _applicationState.Character.Tokens.Count > attribute.CurrentMaxValue)
+                
+                if (attribute.Key == AttributeNames.SIDEBAG)
                 {
-                    var count = _applicationState.Character.Tokens.Count - attribute.CurrentMaxValue;
-                    while (count != 0)
-                    {
-                        _applicationState.Character.Tokens.RemoveAt(_applicationState.Character.Tokens.Count - 1);
-                        count--;
-                    }
+                    SortoutSidebag(attribute);
                 }
             }
 
             _messenger.Send(KeywordMessage.Changed());
 
             return Task.FromResult(true);
+        }
+
+        private void SortoutSidebag(AppLayer.ObservableModels.ObservableAttribute attribute)
+        {
+            if (_applicationState.Character.Tokens.Count > attribute.CurrentMaxValue)
+            {
+                var count = _applicationState.Character.Tokens.Count - attribute.CurrentMaxValue;
+                while (count != 0)
+                {
+                    _applicationState.Character.Tokens.RemoveAt(_random.Next(0, _applicationState.Character.Tokens.Count - 1));
+                    count--;
+                }
+            }
         }
     }
 }
--- src/Infrastructure/Handlers/RefreshAttributeHandler.cs
+++ src/Infrastructure/Handlers/RefreshAttributeHandler.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using RedSpartan.BrimstoneCompanion.AppLayer.Interfaces;
+using RedSpartan.BrimstoneCompanion.AppLayer.ObservableModels;
+using RedSpartan.BrimstoneCompanion.Domain;
 using RedSpartan.BrimstoneCompanion.Infrastructure.Requests;
 
 namespace RedSpartan.BrimstoneCompanion.Infrastructure.Handlers
@@ -7,6 +9,8 @@ namespace RedSpartan.BrimstoneCompanion.Infrastructure.Handlers
     public class RefreshAttributeHandler : IRequestHandler<RefreshAttributesRequest, Unit>
     {
         private readonly IApplicationState _applicationState;
+        private readonly Random _random = new();
+
         public RefreshAttributeHandler(IApplicationState applicationState)
         {
             _applicationState = applicationState ?? throw new ArgumentNullException(nameof(applicationState));
@@ -17,9 +21,28 @@ namespace RedSpartan.BrimstoneCompanion.Infrastructure.Handlers
             {
                 var attribute = _applicationState.Character.Attributes.First(x => x.Key == key);
                 attribute.SetCurrentValues(_applicationState.Character.Features);
+
+                if(attribute.Key == AttributeNames.SIDEBAG)
+                {
+                    SortoutSidebag(attribute);
+                }
             }
 
             return Unit.Task;
+        }
+
+
+        private void SortoutSidebag(ObservableAttribute sidebag)
+        {
+            if (_applicationState.Character.Tokens.Count > sidebag.CurrentMaxValue)
+            {
+                var count = _applicationState.Character.Tokens.Count - sidebag.CurrentMaxValue;
+                while (count != 0)
+                {
+                    _applicationState.Character.Tokens.RemoveAt(_random.Next(0, _applicationState.Character.Tokens.Count - 1));
+                    count--;
+                }
+            }
         }
     }
 }
